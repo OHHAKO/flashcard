715c847fbed20b18a02d96ce05dfef55f6927024
OHHAKO <l36h7129@gmail.com>
2024-02-18 17:41:28 +0900

    feat: 카드 뒷면에서 외움/못외움 버튼을 눌러 다음 카드를 본다 close #6

--- src/data/words.ts
+++ src/data/words.ts
@@ -1,1 +1,9 @@
-export const words = {en: 'hello', ko: '안녕'};
+export const words: {
+  en: string;
+  ko: string;
+}[] = [
+  {en: 'hello', ko: '안녕'},
+  {en: 'dog', ko: '개'},
+  {en: 'cat', ko: '고양이'},
+  {en: 'flower', ko: '꽃'},
+];
